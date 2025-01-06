04091ee77bdde5526b49b6545ed2eeb086f28586
Tao Chen <iamtaochen@outlook.com>
2025-01-07 02:16:51 +0800

    add COPY_AS_DOMAIN

--- scripts/install_cert.sh
+++ scripts/install_cert.sh
@@ -2,15 +2,16 @@
 set -e
 [[ ! -z "$DEBUG" ]] && set -x
 
+# Set ACME_CERTS if not already set
+ACME_CERTS=${ACME_CERTS:-/certs}
+
 FULLCHAIN_FILE=$ACME_CERTS/fullchain.pem
 KEY_FILE=$ACME_CERTS/key.pem
 CERT_FILE=$ACME_CERTS/cert.pem
 CA_FILE=$ACME_CERTS/ca.pem
 RELOAD_SHELL=/after_install_cert.sh
 
-if [ -z "$INCLUDE_WILDCARD" ]; then
-    INCLUDE_WILDCARD="false"
-fi
+INCLUDE_WILDCARD=${INCLUDE_WILDCARD:-"false"}
 DOMAIN_ARG=$(/convert_to_d_args.sh $DOMAINS $INCLUDE_WILDCARD)
 
 acme.sh --install-cert      $DOMAIN_ARG \
@@ -19,3 +20,13 @@ acme.sh --install-cert      $DOMAIN_ARG \
         --key-file          $KEY_FILE \
         --fullchain-file    $FULLCHAIN_FILE \
         --reloadcmd         $RELOAD_SHELL
+
+# Copy the certs as domain.pem and domain.key if we neet
+COPY_AS_DOMAIN=${COPY_AS_DOMAIN:-false}
+if [ "$COPY_AS_DOMAIN" == "true" ]; then
+    for domain in $DOMAINS; do
+        echo "Copying certificates for domain: $domain"
+        cp $FULLCHAIN_FILE $ACME_CERTS/$domain.crt
+        cp $KEY_FILE $ACME_CERTS/$domain.key
+    done
+fi
