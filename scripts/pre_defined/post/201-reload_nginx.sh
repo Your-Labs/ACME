9ceded4dea2d253d943ecf4cb4106a0a5b0c3d1f
Tao Chen <iamtaochen@outlook.com>
2025-01-15 14:52:41 +0800

    fix

--- scripts/pre_defined/post/201-reload_nginx.sh
+++ scripts/pre_defined/post/201-reload_nginx.sh
@@ -9,10 +9,10 @@ ROOT=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 ACME_CONTAINER_NGINX_RELOAD=""
 ACME_DRY_RUN=${ACME_DRY_RUN:-0}
 # ---------------------------------------------------------------
-# Check if RESTART_CONTAINER_ID is set
+# Check if ACME_CONTAINER_NGINX_RELOAD is set
 if [ -z "$ACME_CONTAINER_NGINX_RELOAD" ]; then
     mylog "warn" "ACME_CONTAINER_NGINX_RELOAD is not set, skipping the reload"
     exit 0
 fi
 # call the docker_exec to reload the nginx
-docker_exec $RESTART_CONTAINER_ID "nginx -s reload" "$MYACME_DOCKER_SOCKET" $ACME_DRY_RUN
+docker_exec $ACME_CONTAINER_NGINX_RELOAD "nginx -s reload" "$MYACME_DOCKER_SOCKET" $ACME_DRY_RUN
