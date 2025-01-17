4a8eab844ad2806b14cc41cff1e8dbeeaa505974
Tao Chen <iamtaochen@outlook.com>
2025-01-17 19:34:01 +0800

    cleanup

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,7 @@
-# install the certs manually
-install_certs_dir --config /myacme/issues --run-post-hook
-#  or
+#!/bin/bash
+
+
+# # install the certs manually
+# install_certs_dir --config /myacme/issues --run-post-hook
+# # or
 docker compose exec -T acme install_certs_dir --config /myacme/issues --run-post-hook
